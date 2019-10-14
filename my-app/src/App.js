5f0733f173031c5e6314a0a89a43158567232a70
Kamil Frontuje <kamilwojtalak99@gmail.com>
2019-10-14 14:55:04 +0200

    commit message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import CSS from './App.css';
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Kamil', age: '17' },
+      { name: 'Kornelia', age: '23' },
+      { name: 'Julia', age: '12' },
+    ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, there</h1>
-    </div>
-  );
+  render() {
+    return (
+      <div className='siema'>
+        <h1>Hi, do you can dance? And do you have free Fridays on 13 am.?</h1>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Siema Przykłądowy tekst</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default App
