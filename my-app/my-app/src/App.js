302bcae25d2b5a10e896c31dbe2d3f262c554299
Kamil Frontuje <kamilwojtalak99@gmail.com>
2019-10-22 02:15:17 +0200

    wow

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Person from './Person/Person';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Kamil',
+        age: '17'
+      },
+      {
+        name: 'Szymon',
+        age: '18'
+      }
+    ],
+    showPersons: false
+  }
 
-    </div>
-  );
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !(doesShow)})
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hi, there!</h1>
+        <button onClick={this.togglePersonsHandler}>I am a Baton</button>
+        {
+        this.state.showPersons ?
+          <div>  
+            <Person />
+            <Person />
+            <Person />
+          </div> : null
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ my-app/my-app/src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Person = props => {
+    return (
+        <div className='person'>
+            <p>Lorem Lorem Lorem Lorem Lorem</p>
+            <p>Lorem Lorem Lorem Lorem Lorem</p>
+        </div>
+    )
+}
+
+export default Person;
