f5bf0381007c6c7752c7eb5581c7aa61e3ff19c5
Kamil Frontuje <kamilwojtalak99@gmail.com>
2019-11-30 18:23:52 +0100

    commit message

--- PHP/zielent/index.php
+++ PHP/zielent/index.php
@@ -8,36 +8,20 @@
 </head>
 <body>
     
-<?php
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $animals = [];
-
-    $animal = $_POST['animal'];
-
-    array_push($animals, $animal);
-}
-
-?>
-
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'])?>" method='post'>
-
-    <label for="animal">Podaj zwierze: </label>
-    <input type="text" name="animal" id="animal"> <br /><br />
-
-    <input type="submit" value="Wyślij zamówienie!"> <br />
-</form>
-
-<div class="animals__container"><?php print_r($animals) ?></div>
-
 <?php 
 
-$peopleOne = ['Siema', 'siema'];
-$peopleTwo = ['Siemanoi', 'siemano'];
+function siema($name = 'Kamil') {
+    echo "Siema $name" . '<br/>';
+}
 
-print_r(array_merge($peopleOne, $peopleTwo));
+siema();
+siema('Patryk');
 
 ?>
+
+
+
+
 
 
 </body>
