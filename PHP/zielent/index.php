8de4d8567b4c8e0cbf399688c771eb7b40d45ad1
Kamil Frontuje <kamilwojtalak99@gmail.com>
2019-12-01 23:39:01 +0100

    commit message

--- PHP/zielent/index.php
+++ PHP/zielent/index.php
@@ -4,22 +4,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Zelent poradnik</title>
+    <title>siema</title>
 </head>
 <body>
     
 <?php 
 
-function siema($name = 'Kamil') {
-    echo "Siema $name" . '<br/>';
-}
+$errors = array('email' => 'Błednie wpiałeś mail', 'tekst' => 'Zle! xD');
 
-siema();
-siema('Patryk');
+    if (isset($_POST['submit'])) {
+        $email = htmlspecialchars($_POST['email']);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo "$email nie jest zapisan yw poprawnym formacie";
+        }
+    }
+    if (empty($_POST['siema2'])) {
+        echo 'buuuu';
+    }
 
 ?>
 
-
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="post">
+    <input type="text" name="email" id=""> <br /> <br />
+    <input type="text" name="siema2" id=""> <br /> <br />
+    <input type="text" name="siema3" id=""> <br /> <br />
+    <input type="text" name="siema4" id=""> <br /> <br />
+    <input type="submit" name='submit' value="SIEMA">
+</form>
 
 
 
