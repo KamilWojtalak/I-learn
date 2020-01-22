2e50a82c39c83208acfbd9e29d07af129ba04208
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-01-22 22:15:19 +0100

    Added article.php template to my sandpit

--- PHP/Trying-1/index.php
+++ PHP/Trying-1/index.php
@@ -1,8 +1,23 @@
-
-    <?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
     
-    $randomWord = 'Masz jakiś problem?';
+    $title = 'Witaj świecie, pierwszy blog post!';
+    $description = 'Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum ';
 
-    echo strlen($randomWord);
+    include 'article.php';
+
+    $title = 'Drugi blog post! Pora zacząć przygodę!';
+    $description = 'Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum ';
     
+    include 'article.php';
     ?>
+</body>
+</html>
