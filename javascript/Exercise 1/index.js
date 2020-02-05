8665220fdd654277636219d17cc3ea16e45bbdb2
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-02-06 00:13:35 +0100

    Learning Lab updated folder structure

--- javascript/Exercise 1/index.js
+++ javascript/Exercise 1/index.js
@@ -1,15 +1,32 @@
-let siema = function(declaration, yes, no) {
-    if(+declaration)  yes()
-    else no();
+"use strict";
+
+const siema = function(declaration, yes, no) {
+    if(+declaration)  {yes()}
+    else {no()};
 };
 
-let yes = function() {
+const yes = function() {
     console.log('Siema, niezłe zgadzanko, proste, że tak!');
+    return true;
 };
 
-let no = function() {
+const no = function() {
     console.log('Oj byczku, nie, nie, nie BlooooodTrail');
+    return true;
 };
 
 
-console.log(siema('0', yes, no));
+console.log(siema(1, yes, no));
+
+
+const arrowFucntion = (arg1, arg2) => {
+
+}
+
+describe("pow", function() {
+
+    it('raises to n-th power', function() {
+        assert.equal(pow(2, 3), 8);
+    })
+
+})
--- my-app/my-app/src/siema/Siema.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const siema = props => {
-    return(
-        <div>
-            <h2>Siema</h2>
-        </div>
-    )
-}
