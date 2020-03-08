e242df148c29968ab68b9611bd5b04f387ed3340
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-03-08 23:08:43 +0100

    Added in PHP simply sumOfDigits calculator

---
+++ PHP/Exercise 1/index.php
@@ -0,0 +1,29 @@
+
+<?php
+
+
+function sumOfDigits(int $num) {
+    $sum = 0;
+    $rem = 0;
+
+    while($num > 1) {
+        $rem = $num%10;
+        $sum += $rem;
+        $num = $num/10;
+    }
+    
+
+    echo $sum . '. ';
+    return $sum;
+}
+
+sumOfDigits(42); // 6
+sumOfDigits(666); // 18
+sumOfDigits(4273); // 16
+sumOfDigits(4353453); 
+sumOfDigits('923929393229239'); 
+
+
+
+
+?>
