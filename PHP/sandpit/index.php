b3bdb420eeee7068e0420ad98318dbd86340489b
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-03-23 02:17:11 +0100

    Working with php

--- PHP/sandpit/index.php
+++ PHP/sandpit/index.php
@@ -1,14 +1,24 @@
+<?php
+   $date = getdate();
+?>
+  
   <?php  require("header.php");?>
 
     <div class="container">
         <form action="<? $_SERVER['PHP_SELF'];?>" method="post" class="row d-flex flex-wrap justify-content-center flex-column" enctype="multipart/form-data">
-            <input type="file" name="image" id="image" class="col my-5">
+            <input type="text" name="email" id="email" class="col my-5">
             <input type="submit" value="Siema" class="btn btn-primary">
 
         </form>
         <br>
         <div class="col text-white">
-            <?php print_r($_SERVER); ?>
+            <?php 
+                foreach($date as $key => $value) {
+                    echo "$key: $value" . '<br>';
+                }
+            ?>
+            <br>
+            <?php echo date("m/d/y G.i:s<br>", time());?>
         </div>
     </div>
 </body>
