72bb2eb974a691617af9489ccdc632a7547a557d
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-03-30 00:15:50 +0200

    Added simple form validation in PHP

--- PHP/sandpit/header.php
+++ PHP/sandpit/header.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add something to the database</title>
+    <title>Simple form validation</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="script.js"></script>
 </head>
 <body class="bg-secondary text-white">
 
--- PHP/sandpit/table.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS form(
-   user_id INT NOT NULL AUTO_INCREMENT,
-   user_name VARCHAR(20) NOT NULL,
-   user_password  VARCHAR(20) NOT NULL,
-   user_email   INT NOT NULL,
-   primary key ( user_id ))
