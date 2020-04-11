7062c19b54c4bf25ad41ba4a3ea3636c48568ceb
Kamil Frontuje <kamilwojtalak99@gmail.com>
2020-04-11 03:34:51 +0200

    Working with bootstrap

--- sites/Portfolio Page/Projects/FrontEnd/Cuda Page/js/app.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.header__hamburger-menu');
-const menu = document.querySelector('.title-and-nav__nav');
-
-hamburger.addEventListener('click', (e) => {
-    menu.classList.toggle('hidden');
-}); 
